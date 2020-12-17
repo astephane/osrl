071ab1a19b568d7c91cacc103e64da0d5efeea28
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-12-17 18:28:23 +0100

    Added ECS playground.

---
+++ src/ecs.hpp
@@ -0,0 +1,27 @@
+//
+// Copyright 2020 Stéphane ALBERT
+//
+// This file is part of OSRogueL (osrl).
+//
+// OSRogueL (osrl) is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OSRogueL (osrl) is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with OSRogueL (osrl).  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#ifndef ECS_HPP
+#define ECSL_HPP 0x54470000F233C0D3
+
+// TODO
+
+#endif // ECS_HPP
