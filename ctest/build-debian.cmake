6fb654d3391934e102d27f989ddfdc82e05eebde
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-12-17 12:58:24 +0100

    Fixed linker issue with libtcod.

--- ctest/build-debian.cmake
+++ ctest/build-debian.cmake
@@ -80,8 +80,8 @@ CMAKE_CXX_FLAGS:STRING+= -Wall -Wextra -Wshadow
 CMAKE_INSTALL_PREFIX:STRING=${PRJ_INSTALL_PREFIX}
 
 LIBTCOD_INCLUDE_DIRS:PATH=/usr/include/libtcod
-# LIBTCOD_LIBRARIES=/usr/lib/x86_64-linux-gnu/libtcod.so
-LIBTCOD_LIBRARIES=/usr/lib/x86_64-linux-gnu/libtcod.so;/usr/lib/x86_64-linux-gnu/libtcodgui.so;/usr/lib/x86_64-linux-gnu/libtcodxx.so
+LIBTCOD_LIBRARIES:LIST=/usr/lib/x86_64-linux-gnu/libtcodxx.so
+# LIBTCOD_LIBRARIES:LIST=/usr/lib/x86_64-linux-gnu/libtcod.so\;/usr/lib/x86_64-linux-gnu/libtcodgui.so\;/usr/lib/x86_64-linux-gnu/libtcodxx.so
 ")
 
 set( CTEST_NOTES_FILES
