68e08db21d25955118fb1818a6b0db0dbf46ec49
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-12-18 11:43:16 +0100

    Improved osrl::vec2i tests.

--- test/vec2.test.cpp
+++ test/vec2.test.cpp
@@ -2,29 +2,160 @@
 
 #include "vec2.hpp"
 
-SCENARIO( "", "" )
+SCENARIO( "Zero osrl::vec2i", "" )
 {
-  GIVEN( "A an uninitialized vec2" )
+  GIVEN( "A an uninitialized osrl::vec2i" )
   {
-    osrl::vec2< int > v;
+    osrl::vec2i u;
 
-    THEN( "" )
+    THEN( "All components must be zero" )
     {
-      CAPTURE( v );
-      CAPTURE( v.x );
-      CAPTURE( v.y );
+      CAPTURE( u );
+      CAPTURE( u.x );
+      CAPTURE( u.y );
 
-      REQUIRE( v.x == 0 );
-      REQUIRE( v.y == 0 );
+      REQUIRE( u.x == 0 );
+      REQUIRE( u.y == 0 );
+
+      AND_THEN( "osrl::vec2i:is_zero() must be true" )
+      {
+	CAPTURE( u );
+	CAPTURE( u.x );
+	CAPTURE( u.y );
+	CAPTURE( u.is_zero() );
+
+	REQUIRE( u.is_zero() );
+      }
     }
+  }
+}
 
-    THEN( "" )
+SCENARIO( "Construction of osrl::vec2i", "" )
+{
+  GIVEN( "An initialized osrl::vec2i" )
+  {
+    osrl::vec2i u( -1, +2 );
+
+    THEN( "All components must match their initialization value" )
     {
-      CAPTURE( v );
-      CAPTURE( v.x );
-      CAPTURE( v.y );
+      CAPTURE( u );
+      CAPTURE( u.x );
+      CAPTURE( u.y );
 
-      REQUIRE( v.is_zero() );
+      REQUIRE( u.x == -1 );
+      REQUIRE( u.y == +2 );
+
+      AND_THEN( "osrl::vec2i:is_zero() must be false" )
+      {
+	CAPTURE( u );
+	CAPTURE( u.x );
+	CAPTURE( u.y );
+	CAPTURE( u.is_zero() );
+
+	REQUIRE( !u.is_zero() );
+      }
+    }
+  }
+}
+
+SCENARIO( "Addition of osrl::vec2i", "" )
+{
+  GIVEN( "A an uninitialized vec2i" )
+  {
+    osrl::vec2i u;
+
+    AND_GIVEN( "Another non-zero osrl::vec2i" )
+    {
+      osrl::vec2i v( -1, +2 );
+
+      AND_GIVEN( "The addition of both osrl::vec2i" )
+      {
+	osrl::vec2i w = u + v;
+
+	THEN( "" )
+	{
+	  CAPTURE( w );
+	  CAPTURE( w.x );
+	  CAPTURE( w.y );
+
+	  REQUIRE( w.x == -1 );
+	  REQUIRE( w.y == +2 );
+
+	  AND_THEN( "" )
+	  {
+	    CAPTURE( w );
+	    CAPTURE( w.x );
+	    CAPTURE( w.y );
+	    CAPTURE( w.is_zero() );
+
+	    REQUIRE( !w.is_zero() );
+	  }
+	}
+      }
+    }
+  }
+}
+
+
+SCENARIO( "Negation of osrl::vec2i", "" )
+{
+  GIVEN( "An uninitialized osrl::vec2i" )
+  {
+    osrl::vec2i u;
+
+    AND_GIVEN( "Its negation" )
+    {
+      osrl::vec2i v = -u;
+
+      THEN( "" )
+      {
+	CAPTURE( v );
+	CAPTURE( v.x );
+	CAPTURE( v.y );
+
+	REQUIRE( v.x == 0 );
+	REQUIRE( v.y == 0 );
+
+	AND_THEN( "" )
+	{
+	  CAPTURE( v );
+	  CAPTURE( v.x );
+	  CAPTURE( v.y );
+	  CAPTURE( v.is_zero() );
+
+	  REQUIRE( v.is_zero() );
+	}
+      }
+    }
+  }
+
+  GIVEN( "An initialized osrl::vec2i" )
+  {
+    osrl::vec2i u( -1, +2 );
+
+    AND_GIVEN( "Its negation" )
+    {
+      osrl::vec2i v = -u;
+
+      THEN( "" )
+      {
+	CAPTURE( v );
+	CAPTURE( v.x );
+	CAPTURE( v.y );
+
+	REQUIRE( v.x == +1 );
+	REQUIRE( v.y == -2 );
+
+	AND_THEN( "" )
+	{
+	  CAPTURE( v );
+	  CAPTURE( v.x );
+	  CAPTURE( v.y );
+	  CAPTURE( v.is_zero() );
+
+	  REQUIRE( !v.is_zero() );
+	}
+      }
     }
   }
 }
